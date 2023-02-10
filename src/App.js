e1348e12be556daf8edc4c69b60f1f8901ac6b60
Lexi Percival <admin@v-metrics.com>
2023-02-10 10:50:55 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Papa from "papaparse";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [carData, setCardata] = useState([]);
+  const [year, setYear] = useState(null);
+
+  useEffect(() => {
+    console.log(carData);
+    // for (let i = 1; i < carData.length; i++) {
+    //   console.log(carData[i]);
+    // }
+  }, [carData]);
+
+  function yearChange(e) {
+    setYear(e.target.value);
+  }
+
+  function filterCarlist() {
+    for (let i = 1; i < carData.length; i++) {
+      if (carData[i][0] === year) {
+        console.log(carData[i]);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        accept=".csv,.xlsx,.xls, .txt"
+        onChange={(e) => {
+          const files = e.target.files;
+          console.log(files);
+          if (files) {
+            console.log(files[0]);
+            Papa.parse(files[0], {
+              complete: function (results) {
+                setCardata(results.data);
+              },
+            });
+          }
+        }}
+      />
+      <input
+        type="number"
+        onChange={(e) => {
+          yearChange(e);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          filterCarlist();
+        }}
+      >
+        Console cars
+      </button>
     </div>
   );
 }
